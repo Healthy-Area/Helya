9661c12b4bdcd38123c689d465f69ac15b785817
Yulius Pune <109933512+Oxcyy@users.noreply.github.com>
2025-11-04 22:35:27 +0800

    Create tipsOtomatisDao.java

---
+++ Src/model/dao/tipsOtomatisDao.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.dao;
+
+/**
+ *
+ * @author Aspire5
+ */
+import model.entity.tipsOtomatis;
+
+public class tipsOtomatisDao extends genericDAO<tipsOtomatis, String> {
+    public tipsOtomatisDao() { super(tipsOtomatis.class); }
+}
