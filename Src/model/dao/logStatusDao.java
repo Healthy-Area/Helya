9af133a109d72b2ef692a5cac5e506a6ee5f5606
Yulius Pune <109933512+Oxcyy@users.noreply.github.com>
2025-11-04 22:33:55 +0800

    Create logStatusDao.java

---
+++ Src/model/dao/logStatusDao.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.dao;
+
+/**
+ *
+ * @author Aspire5
+ */
+import model.entity.logStatus;
+
+public class logStatusDao extends genericDAO<logStatus, String> {
+    public logStatusDao() { super(logStatus.class); }
+}
